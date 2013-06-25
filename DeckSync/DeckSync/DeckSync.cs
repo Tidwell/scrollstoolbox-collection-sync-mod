d4d5b5e79a4be51bbce7ca4cc55706e176d615a0
Aaron Tidwell <aaron.tidwell@gmail.com>
2013-06-24 21:13:42 -0400

    first

--- DeckSync/DeckSync/DeckSync.cs
+++ DeckSync/DeckSync/DeckSync.cs
@@ -15,19 +15,20 @@ using UnityEngine;
 
 namespace DeckSync
 {
-    public class DeckSync : BaseMod, IOkStringCancelCallback
+    public class DeckSync : BaseMod
     {
 
         private DeckBuilder2 deckBuilder = null;
 
         public static bool loaded = false;
-
-        private bool inited = false;
-        private Dictionary<long, Card> allCardsDict = null;
-
-        private Type deckBuilderType = typeof(DeckBuilder2);
+		private bool inited = false;
 
 		private GUISkin buttonSkin = (GUISkin)Resources.Load("_GUISkins/Lobby");
+
+		private Dictionary<long, Card> allCardsDict = null;
+		private Type deckBuilderType = typeof(DeckBuilder2);
+		private string username = "";
+		private string ingamename = "";
 
         public override void AfterInvoke(InvocationInfo info, ref object returnValue)
         {
@@ -41,11 +42,35 @@ namespace DeckSync
 				GUI.skin = buttonSkin;
                 GUIPositioner positioner3 = App.LobbyMenu.getSubMenuPositioner(1f, 5);
                 GUI.skin = buttonSkin;
-                if (LobbyMenu.drawButton(positioner3.getButtonRect(3f), "Import Deck"))
+                if (LobbyMenu.drawButton(positioner3.getButtonRect(3f), "Sync Collection"))
                 {
-                    App.Popups.ShowTextInput(this, "", "", "impdeck", "Import deck", "Insert the link to your deck:", "Import");
+					FieldInfo initedInfo = deckBuilderType.GetField("inited", BindingFlags.NonPublic | BindingFlags.Instance);
+
+					inited = (bool)initedInfo.GetValue(deckBuilder);
+					if (inited)
+					{
+						FieldInfo deckListInfo = deckBuilderType.GetField("allCardsDict", BindingFlags.NonPublic | BindingFlags.Instance);
+						allCardsDict = (Dictionary<long, Card>)deckListInfo.GetValue(deckBuilder);
+					}
+
+					var writer = new JsonWriter();
+					string json = writer.Write(allCardsDict);
+
+					this.loadFromWeb(json);
                 }
             }
+
+			if (info.targetMethod.Equals("login"))
+			{
+				this.username = (string)typeof(Login).GetField ("username", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(info.target);
+
+			}
+
+			if (info.targetMethod.Equals("profileinfomessage"))
+			{
+				this.ingamename = (string)typeof(ProfileInfo).GetField ("name", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(info.target);
+
+			}
         }
 
         public override bool BeforeInvoke(InvocationInfo info, out object returnValue)
@@ -59,7 +84,9 @@ namespace DeckSync
 			try
 			{
 				return new MethodDefinition[] {
-						scrollsTypes["DeckBuilder2"].Methods.GetMethod("OnGUI")[0] // to draw gui buttons on the deckbuilder screen
+						scrollsTypes["DeckBuilder2"].Methods.GetMethod("OnGUI")[0], // to draw gui buttons on the deckbuilder screen
+						scrollsTypes["Login"].Methods.GetMethod("login")[0], //this may seem scary, but i swear im just getting the username
+						scrollsTypes["ProfileInfoMessage"].Methods.GetMethod("profileinfomessage")[0]
 				};
 			}
 			catch
@@ -70,7 +97,7 @@ namespace DeckSync
 
         public static string GetName()
         {
-            return "DeckSync";
+            return "CollectionSync";
         }
 
         public static int GetVersion()
@@ -83,124 +110,18 @@ namespace DeckSync
 
         }
 
-        public void PopupOk(string popupType, string choice)
-        {
-            if (popupType == "impdeck")
-            {
-                // try a few regex patterns until a match is found
-                List<RegexPattern> patterns = new List<RegexPattern>();
-                patterns.Add(new RegexPattern("^(http://)?www\\.scrollsguide\\.com/deckbuilder/\\?d=([0-9]+)$", 2));
-                patterns.Add(new RegexPattern("^(http://)?www\\.scrollsguide\\.com/deckbuilder/?#([0-9]+)$", 2)); // not sure whether the / after deckbuilder is required...
-                patterns.Add(new RegexPattern("^#?([0-9]+)$", 1));
-
-                bool hasMatch = false;
-                for (int i = 0; i < patterns.Count && !hasMatch; i++)
-                {
-                    RegexPattern pattern = patterns[i];
-
-                    Match m = Regex.Match(choice, pattern.getPattern());
-                    if (m.Success)
-                    {
-                        hasMatch = true;
-
-                        FieldInfo initedInfo = deckBuilderType.GetField("inited", BindingFlags.NonPublic | BindingFlags.Instance);
-
-                        inited = (bool)initedInfo.GetValue(deckBuilder);
-                        if (inited)
-                        {
-                            FieldInfo deckListInfo = deckBuilderType.GetField("allCardsDict", BindingFlags.NonPublic | BindingFlags.Instance);
-                            allCardsDict = (Dictionary<long, Card>)deckListInfo.GetValue(deckBuilder);
-
-                            loadFromWeb(m.Groups[pattern.getMatchNum()].Value);
-                        }
-                    }
-                }
-                if (!hasMatch)
-                {
-                    App.Popups.ShowOk(null, "fail", "Invalid deck", "That is not a valid link to your deck.", "Ok");
-                }
-            }
-        }
-
-        private void loadFromWeb(String deckId)
+		private void loadFromWeb(String collectionData)
         {
 
 			WebClientTimeOut wc = new WebClientTimeOut();
 			wc.DownloadStringCompleted += (sender, e) =>
 			{
-				processDeck(e.Result);
+				App.Popups.ShowOk(null, "fail", "Import failed", "That deck does not exist, or is deleted.", "Ok");
 			};
 			wc.TimeOut = 5000;
-			wc.DownloadStringAsync(new Uri("http://a.scrollsguide.com/deck/load?id=" + deckId));
+			wc.DownloadStringAsync(new Uri("http://localhost:9000/collection/update?ingamename="+this.ingamename+"&inGameName="+this.username+"&data=" + collectionData));
         }
+	
+	}
 
-		private void processDeck(String result)
-		{
-			JsonReader reader = new JsonReader();
-
-			try
-			{
-				ApiDeckMessage adm = reader.Read(result, System.Type.GetType("ApiDeckMessage")) as ApiDeckMessage;
-
-				if (adm.msg.Equals("success"))
-				{
-					List<long> toPlaceOnBoard = new List<long>();
-					foreach (KeyValuePair<long, Card> singleScroll in allCardsDict)
-					{
-						//log.WriteLine("Checking " + singleScroll.Value.getName() + " ...");
-						for (int i = 0; i < adm.data.scrolls.Length; i++)
-						{
-							DeckScroll d = adm.data.scrolls[i];
-							//log.WriteLine("Comparing " + d.id + " to " + singleScroll.Value.getCardType().id + " ...");
-							if (singleScroll.Value.getCardType().id == d.id && d.c > 0 && !toPlaceOnBoard.Contains(singleScroll.Key)) // this scroll needs to be added to the deck still
-							{
-								d.c--;
-								toPlaceOnBoard.Add(singleScroll.Key);
-								// log.WriteLine("Added to toPlaceOnBoard");
-							}
-						}
-					}
-
-					if (toPlaceOnBoard.Count > 0)
-					{
-						MethodInfo mo = deckBuilderType.GetMethod("loadDeck", BindingFlags.NonPublic | BindingFlags.Instance);
-						mo.Invoke(deckBuilder, new object[] { (int)-1, (List<long>)toPlaceOnBoard, null });
-
-						// mo = deckBuilderType.GetMethod("alignTableCards", BindingFlags.NonPublic | BindingFlags.Instance);
-						// mo.Invoke(deckBuilder, new object[]{(int)1});
-					}
-				}
-				else
-				{
-					App.Popups.ShowOk(null, "fail", "Import failed", "That deck does not exist, or is deleted.", "Ok");
-				}
-			}
-			catch  // just... general fail
-			{
-				App.Popups.ShowOk(null, "fail", "Import failed", "That deck does not exist, or is deleted.", "Ok");
-			}
-		}
-
-    }
-
-    internal class RegexPattern
-    {
-        private String pattern;
-        private int matchNum;
-
-        public RegexPattern(String pattern, int matchNum)
-        {
-            this.pattern = pattern;
-            this.matchNum = matchNum;
-        }
-
-        public String getPattern()
-        {
-            return this.pattern;
-        }
-        public int getMatchNum()
-        {
-            return this.matchNum;
-        }
-    }
 }
